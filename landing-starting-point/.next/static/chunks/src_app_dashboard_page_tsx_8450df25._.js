4a1d031ff8b263997d609440523ab020820d94a1
PericoPe <javierhursino@gmail.com>
2025-04-23 22:06:47 -0300

    Corrijo .gitignore y elimino archivos pesados y temporales del repo. Ahora el push será rápido y sin errores de memoria.

--- landing-starting-point/.next/static/chunks/src_app_dashboard_page_tsx_8450df25._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_65a5c473._.js"
-],
-    source: "dynamic"
-});
